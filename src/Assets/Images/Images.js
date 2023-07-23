216b5d30f0930f41623d2954e24c3c6195118840
Amutha Muhunthan <tamamutha@gmail.com>
2023-07-23 19:54:22 +1000

    added 2 more images and change banner image

--- src/Assets/Images/Images.js
+++ src/Assets/Images/Images.js
@@ -10,56 +10,62 @@
 export const NatureImages = [
   {
     id: 1,
+    name: 'Lighthouse in Wollongong',
+    size: '40cm(W) x 51cm(H)',
+    image: require('./nature/LighthouseWollongong.jpg').default,
+  },
+  {
+    id: 2,
     name: 'Wisteria Blooms',
     size: '76cm(W) x 60cm(H)',
     image: require('./nature/wisteriablooms.jpg').default,
   },
   {
-    id: 2,
+    id: 3,
     name: 'Caves beach',
     size: '75cm(W) x 50cm(H)',
     image: require('./nature/cavesbeach.jpg').default,
   },
 
   {
-    id: 3,
+    id: 4,
     name: 'Minnamurra Falls',
     size: '23cm(W) x 30cm(H)',
     image: require('./nature/minnamurrafalls.jpg').default,
   },
   {
-    id: 4,
+    id: 5,
     name: 'Glistening Waters',
     size: '30.5cm(W) x 23cm(H)',
     image: require('./nature/glisteningwaters.jpg').default,
   },
 
   {
-    id: 5,
+    id: 6,
     name: 'Scenic Waters Hawaii',
     size: '30cm(W) x 30cm(H)',
     image: require('./nature/scenicwatershawaii.jpg').default,
   },
   {
-    id: 6,
+    id: 7,
     name: 'Shining Gum Tree',
     size: '40cm(W) x 30cm(H)',
     image: require('./nature/shininggumtree.jpg').default,
   },
   {
-    id: 7,
+    id: 8,
     name: 'The Lighthouse Wollongong',
     size: '18cm(W) x 13cm(H)',
     image: require('./nature/thelighthousewollongong.jpeg').default,
   },
   {
-    id: 8,
+    id: 9,
     name: 'Serene Waters',
     size: '120cm(W) x 90cm(H)',
     image: require('./nature/serenewaters.jpg').default,
   },
   {
-    id: 9,
+    id: 10,
     name: 'Beside Still Waters',
     size: '50cm(W) x 40cm(H)',
     image: require('./nature/besidestillwaters.jpg').default,
